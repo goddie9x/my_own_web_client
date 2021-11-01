92a985c9f6271d62d0722d385b77dc64223a413d
goddie9x <goddie9x@yahoo.com>
2021-11-01 19:19:21 +0700

    fix move to top

--- src/Component/Common/MoveToTop.jsx
+++ src/Component/Common/MoveToTop.jsx
@@ -5,14 +5,17 @@ import Button from "@mui/material/Button";
 
 export default function MoveToTop() {
   const [showMoveToTopBtn, setShowMoveToTopBtn] = React.useState(false);
+  function moveToTop() {
+    window.scrollTo(20, 20);
+  }
 
-  function handleDisplayMoveToTop() {
-    setShowMoveToTopBtn(window.scrollY > 200);
-  }
-  function moveToTop(){
-    window.scrollTo(20,20);
-  }
   React.useEffect(() => {
+    function handleDisplayMoveToTop() {
+      let isShowMoveToTop = window.scrollY > 200;
+
+      setShowMoveToTopBtn(isShowMoveToTop);
+    }
+
     window.addEventListener("scroll", handleDisplayMoveToTop);
 
     return () => {
@@ -21,16 +24,15 @@ export default function MoveToTop() {
   });
 
   return (
-    <>
-      {showMoveToTopBtn && (
-        <div className="move-to-top btn">
-          <Button variant="contained" 
-          sx={{backgroundColor:"#0a1929"}}
-          onClick={moveToTop}>
+      <div className="move-to-top btn">
+          {showMoveToTopBtn&&<Button
+            variant="contained"
+            sx={{ backgroundColor: "#0a1929" }}
+            onClick={moveToTop}
+          >
             <KeyboardArrowUpSharpIcon />
           </Button>
+        }
         </div>
-      )}
-    </>
   );
 }
